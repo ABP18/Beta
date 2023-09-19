a0064d346e02c04c3ed6f55cf9c83f1dddcc0616
ALEX <ALEX@DESKTOP-GLEMG17>
2023-09-19 22:49:14 +0200

    1.0.3

--- Beta1/src/Menu.java
+++ Beta1/src/Menu.java
@@ -4,6 +4,7 @@ public class Menu {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  System.out.println("hola mundo");
+ System.err.println("dasdasdasdasda");
 	}
 
 }
